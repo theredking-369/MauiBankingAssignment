f8c555524240533056d7c0012168d092576e8c83
theredking-369 <richardsrory3@gmail.com>
2025-08-12 14:24:07 +0200

    1

---
+++ MauiBankingExercise/Services/BankingDatabaseService.cs
@@ -0,0 +1,26 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiBankingExercise.Services
+{
+    public class BankingDatabaseService
+    {
+        private SQLiteConnection _dbConnection;
+
+        public string GetDatabasePath()
+        {
+            string filename = "banking.db";
+            string pathToDb = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            return Path.Combine(pathToDb, filename);
+        }
+
+        public BankingDatabaseService()
+        {
+                
+        }
+    }
+}
