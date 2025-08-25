cd3f777dfe65ee91b54e36732b80120bdf217eff
theredking-369 <richardsrory3@gmail.com>
2025-08-25 14:36:36 +0200

    .

--- MauiBankingExercise/ViewModels/AllCustomersViewModel.cs
+++ MauiBankingExercise/ViewModels/AllCustomersViewModel.cs
@@ -1,12 +1,22 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace MauiBankingExercise.ViewModels
 {
-    internal class AllCustomersViewModel
+    public class AllCustomersViewModel
     {
+       
+        
+
+        public AllCustomersViewModel()
+        {
+
+        }
+
     }
 }
